66b700cc0fa9969b7ed49b538aadcaad4bdc8da8
TheFloyds4240 <80289503+TheFloyds4240@users.noreply.github.com>
2021-09-13 17:24:00 -0400

    Update Script
    Implement new configs system, and add additional compatibility for more Linux Distros

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if grep -q "setup=true" "deepslate.yml" &> /dev/null; then
+if grep -q "setup=true" "deepslateconfig/setup.yml" &> /dev/null; then
 echo "Thank You For Using DeepslateMC, Please Wait While We Make Sure You Are Up To Date"
 curl -o server.jar -z server.jar https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar &> /dev/null
 cd plugins
@@ -12,10 +12,12 @@ curl -o GeyserOptionalPack.mcpack -z GeyserOptionalPack.mcpack https://ci.openco
 cd ..
 cd ..
 cd ..
-echo "The Server Is Now Starting, Happy Gaming!"
-java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -DGeyserSkinManager.ForceShowSkins=true -jar server.jar nogui1~
+source deepslateconfig/ram.yml
+echo "The Server Is Now Starting With "$ram" Gigabytes Of RAM, Happy Gaming!"
+source deepslateconfig/ram.yml
+java -Xms"$ram"G -Xmx"$ram"G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -DGeyserSkinManager.ForceShowSkins=true -jar server.jar nogui1~
 else
-if grep -q "setup=false" "deepslate.yml" &> /dev/null; then
+if grep -q "setup=false" "deepslateconfig/setup.yml" &> /dev/null; then
 echo "Thank You For Using DeepslateMC, Please Wait While We Finish Setting Up Your Server"
 sed -i 's/  server-mod-name: Purpur/  server-mod-name: DeepslateMC/g' purpur.yml &> /dev/null
 curl -o server.jar -z server.jar https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar &> /dev/null
@@ -36,12 +38,16 @@ curl -o GeyserOptionalPack.mcpack -z GeyserOptionalPack.mcpack https://ci.openco
 cd ..
 cd ..
 cd ..
-echo "setup=true" &> deepslate.yml
 echo "The Server Is Now Setup, Happy Gaming!"
-java -Xms4G -Xmx4G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -DGeyserSkinManager.ForceShowSkins=true -jar server.jar nogui1~
+rm deepslateconfig/setup.yml
+echo "setup=true" &> deepslateconfig/setup.yml
+source deepslateconfig/ram.yml
+echo Starting The Server With "$ram" Gigabytes of RAM
+source deepslateconfig/ram.yml
+java -Xms"$ram"G -Xmx"$ram"G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -DGeyserSkinManager.ForceShowSkins=true -jar server.jar nogui1~
 else
 echo "Thank You For Using Deepslate MC, Please Wait While We Initialize Your Server!"
-echo "Remember To Reboot Your Server After Initial Setup To Allow Us To Finish Setting It Up"
+echo "Remember To Reboot Your Server After The Initialization So We Can Finish Setting It Up"
 echo "eula=true" &> eula.txt
 curl -o server.jar https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar &> /dev/null
 mkdir plugins &> /dev/null
@@ -57,8 +63,12 @@ curl -o GeyserOptionalPack.mcpack https://ci.opencollab.dev/job/GeyserMC/job/Gey
 cd ..
 cd ..
 cd ..
-echo "setup=false" &> deepslate.yml
+mkdir deepslateconfig &> /dev/null
+cd deepslateconfig &> /dev/null
+echo "setup=false" &> setup.yml
+echo "ram=2" &> ram.yml
+cd ..
 java -jar server.jar nogui
-echo "Deepslate Initialization Complete, Starting Server. Remember To Reboot!"
+echo "Deepslate Initialization Complete, Remember To Reboot!"
 fi
 fi
